f2a5c59010fe0fd0fb6d73a5cef86a74ab858728
Eunjung Nam <eunjungnam@eunjungs-air.lan>
2021-03-08 12:21:51 -0500

    add winning result text dynamically using if statement

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+//variables
 const player1 = document.querySelector(".img1");
 const player2 = document.querySelector(".img2");
+const headerText = document.querySelector("h1");
 
 //generate random number
 const randomNumber1 = Math.floor(Math.random() * 6) + 1;
@@ -8,3 +10,12 @@ const randomNumber2 = Math.floor(Math.random() * 6) + 1;
 //change dice images
 player1.src = `./images/dice${randomNumber1}.png`;
 player2.src = `./images/dice${randomNumber2}.png`;
+
+//winner text
+if (randomNumber1 > randomNumber2) {
+  headerText.innerText = "🚩 Player1 wins!";
+} else if (randomNumber2 > randomNumber2) {
+  headerText.innerText = "Player2 wins! 🚩";
+} else {
+  headerText.innerText = "Draw! ";
+}
