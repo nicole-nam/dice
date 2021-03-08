127508f7fa60289f9e5f9f8f0a79fc482f73e207
Eunjung Nam <eunjungnam@eunjungs-air.lan>
2021-03-08 12:11:26 -0500

    dchange dice images dynamically using random number

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+const player1 = document.querySelector(".img1");
 
-const randomNumber1 = Math.floor(Math.random() * 7);
+//generate random number
+const randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
-console.log(randomNumber1);
+//change dice images
+player1.src = `./images/dice${randomNumber1}.png`;
