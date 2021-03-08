e8dc549f2367dd504733b90181612df988fc0518
Eunjung Nam <eunjungnam@eunjungs-air.lan>
2021-03-08 12:13:00 -0500

    change player2 dice images dynamically using randomNumber2

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const player1 = document.querySelector(".img1");
+const player2 = document.querySelector(".img2");
 
 //generate random number
 const randomNumber1 = Math.floor(Math.random() * 6) + 1;
+const randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
 //change dice images
 player1.src = `./images/dice${randomNumber1}.png`;
+player2.src = `./images/dice${randomNumber2}.png`;
